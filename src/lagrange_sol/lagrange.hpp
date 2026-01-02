bfdcc30885ff32f5ebfe030b455abaad624633f0
JabelResendiz <jabelresendiz00@gmail.com>
2026-01-01 21:49:55 -0500

    fix lagrange

--- src/lagrange_sol/lagrange.hpp
+++ src/lagrange_sol/lagrange.hpp
@@ -6,50 +6,7 @@
 #include "../utils/graph.hpp"
 #include "../utils/dsu.hpp"
 #include "../utils/edge.hpp"
-
-/* ========================================
-   Estructura para el árbol generador
-   ======================================== */
-struct SpanningTree {
-    vEdges edges;
-    vi degree;
-    int n;
-
-    SpanningTree(int n) : n(n), degree(n, 0) {}
-
-    void addEdge(const Edge& e) {
-        edges.push_back(e);
-        degree[e.u]++;
-        degree[e.v]++;
-    }
-
-    void removeEdge(int u, int v) {
-        for (auto it = edges.begin(); it != edges.end(); ++it) {
-            if ((it->u == u && it->v == v) || (it->u == v && it->v == u)) {
-                degree[it->u]--;
-                degree[it->v]--;
-                edges.erase(it);
-                return;
-            }
-        }
-    }
-
-    ll cost() const {
-        ll total = 0;
-        for (const auto& e : edges) {
-            total += e.w;
-        }
-        return total;
-    }
-
-    bool isFeasible(const vi& deg_limit) const {
-        if ((int)edges.size() != n - 1) return false;
-        for (int i = 0; i < n; i++) {
-            if (degree[i] > deg_limit[i]) return false;
-        }
-        return true;
-    }
-};
+#include "spanningTree.hpp"
 
 /* ========================================
    Cálculo de saturación de un árbol
